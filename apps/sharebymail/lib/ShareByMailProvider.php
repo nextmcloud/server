942223373df72b5a301c4d9a5997a45a91ab4124
memurats <Mauro-Efisio.Mura@t-systems.com>
2025-07-24 08:28:20 +0000

    removed listitems added body text

--- apps/sharebymail/lib/ShareByMailProvider.php
+++ apps/sharebymail/lib/ShareByMailProvider.php
@@ -332,7 +332,17 @@ class ShareByMailProvider extends DefaultShareProvider implements IShareProvider
 		$emailTemplate->setSubject($this->l->t('%1$s shared %2$s with you', [$initiatorDisplayName, $filename]));
 		$emailTemplate->addHeader();
 		$emailTemplate->addHeading($this->l->t('%1$s shared %2$s with you', [$initiatorDisplayName, $filename]), false);
+		$text = $this->l->t('%1$s shared »%2$s« with you.', [$initiatorDisplayName, $filename]);
 
+		if ($note !== '') {
+			$emailTemplate->addBodyText(htmlspecialchars($note), $note);
+		}
+		$emailTemplate->addBodyText(
+			htmlspecialchars($text . ' ' . $this->l->t('Click the button below to open it.')),
+			$text
+		);
+
+		/*
 		if ($note !== '') {
 			$emailTemplate->addBodyListItem(
 				htmlspecialchars($note),
@@ -350,6 +360,7 @@ class ShareByMailProvider extends DefaultShareProvider implements IShareProvider
 				$this->getAbsoluteImagePath('caldav/time.png'),
 			);
 		}
+		*/
 
 		$emailTemplate->addBodyButton(
 			$this->l->t('Open %s', [$filename]),
