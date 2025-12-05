979d2c850249fcc24d45a1ba03b7b50501676f43
Mauro Mura <Mauro-Efisio.Mura@t-systems.com>
2025-12-05 13:42:41 +0100

    Change getL10nFilesForApp to public visibility

--- lib/private/L10N/Factory.php
+++ lib/private/L10N/Factory.php
@@ -555,7 +555,7 @@ class Factory implements IFactory {
 	 *
 	 * @return string[]
 	 */
-	private function getL10nFilesForApp(string $app, string $lang): array {
+	public function getL10nFilesForApp(string $app, string $lang): array {
 		$languageFiles = [];
 
 		$i18nDir = $this->findL10nDir($app);
